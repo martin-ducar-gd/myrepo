4ed49c8ded32b579ee2039e66b88cdb0595b7c59
Martin Ducar <martin.ducar@gooddata.com>
2014-02-11 16:50:38 +0100

    fix

--- fstest.sh
+++ fstest.sh
@@ -14,8 +14,6 @@ shift
 cdir=$(pwd)
 fstest_dir=/opt/fstest
 (( $# <1 )) && usage
-total_f=0
-total_p=0
 ts=$(date +%s)
 while (( ts + t >= $(date +%s) )); do
   d=$(date +%F_%T)
@@ -25,8 +23,8 @@ while (( ts + t >= $(date +%s) )); do
   for mount in $@; do
     mkdir -p $results_dir/fstest_run/$d/$mount || exit 1
     cd $mount
-    mkdir -p $mount/$(hostname)
-    cd $mount/$(hostname)
+    mkdir -p $mount/$(hostname)/fstest
+    cd $mount/$(hostname)/fstest
     exec > $results_dir/fstest_run/$d/$mount/results.out 2>&1
     for i in $(find $fstest_dir/tests/* -type d -print); do
       for j in $(ls -1 $i/*); do
@@ -38,8 +36,6 @@ while (( ts + t >= $(date +%s) )); do
     passed=$(grep "ok" $results_dir/fstest_run/$d/$mount/results.out | wc -l)
     echo "Status, failed tests: $failed, passed tests: $passed"
     echo "Status, failed tests: $failed, passed tests: $passed, for mount: $mount" >> $results_dir/fstest_run/$d/results.out
-    (( total_f=$total_f+$failed ))
-    (( total_p=$total_p+$passed ))
   done
 done
 cd $cdir
